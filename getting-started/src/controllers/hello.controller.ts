64afb462ffc3d061091399870affbeb13446e6ba
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2018-12-20 22:46:34 -0500

    POST SAMPLE
    POST SAMPLE

--- getting-started/src/controllers/hello.controller.ts
+++ getting-started/src/controllers/hello.controller.ts
@@ -1,11 +1,16 @@
 // Uncomment these imports to begin using these cool features!
 
 // import {inject} from '@loopback/context';
-import {get} from '@loopback/rest';
+import {get, post} from '@loopback/rest';
 
 export class HelloController {
   @get('/hello')
   hello(): string {
     return 'Hello world!';
+  }
+
+  @post('/hello')
+  helloPost(value: string): string {
+    return 'SERIO' + value;
   }
 }
