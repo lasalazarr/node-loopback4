88597663594ed9962cfe2c10aaa00807d51dda38
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2019-01-01 20:50:30 -0500

    Clean code

--- getting-started/src/controllers/hello.controller.ts
+++ getting-started/src/controllers/hello.controller.ts
@@ -1,16 +1,11 @@
 // Uncomment these imports to begin using these cool features!
 
-// import {inject} from '@loopback/context';
+import {inject} from '@loopback/context';
 import {get, post, param} from '@loopback/rest';
 
 export class HelloController {
   @get('/hello')
   hello(): string {
     return 'Hello world!';
-  }
-
-  @post('/hello')
-  helloPost(value: string): string {
-    return 'SERIO' + value;
   }
 }
